f923242ef72383f4cab2f0974b1ca85a41a5397d
mongmong2 <luv1004_i@naver.com>
2016-01-24 09:11:52 +0900

    잔액bar 고정

--- app.js
+++ app.js
@@ -13,4 +13,25 @@ angular
     $routeProvider.otherwise({redirectTo: '/view1'});
   }])
   .controller('AppCtrl', function($scope) {
-  });;
+    $scope.showAdvanced = function(ev) {
+      var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+      $mdDialog.show({
+        controller: DialogController,
+        templateUrl: 'dialog1.tmpl.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true,
+        fullscreen: useFullScreen
+      })
+        .then(function(answer) {
+          $scope.status = 'You said the information was "' + answer + '".';
+        }, function() {
+          $scope.status = 'You cancelled the dialog.';
+        });
+      $scope.$watch(function() {
+        return $mdMedia('xs') || $mdMedia('sm');
+      }, function(wantsFullScreen) {
+        $scope.customFullscreen = (wantsFullScreen === true);
+      });
+    };
+  });
