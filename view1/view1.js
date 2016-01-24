20602fda14c6109061409e8afc214874fd00c8e7
mongmong2 <luv1004_i@naver.com>
2016-01-24 10:40:39 +0900

    모임 상세보기 구현

--- view1/view1.js
+++ view1/view1.js
@@ -2,13 +2,51 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl'
+    });
+  }])
+  .controller('View1Ctrl', ['$scope', '$mdMedia', '$mdDialog', function ($scope, $mdMedia, $mdDialog) {
+    $scope.showDetail = function(ev) {
+      $mdDialog.show({
+        controller: DialogController,
+        templateUrl: 'meetingDetail.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose: true,
+        fullscreen: true
+      })
+    };
 
-.controller('View1Ctrl', [function() {
+    $scope.showAdvanced = function (ev) {
+      $mdDialog.show({
+        controller: DialogController,
+        templateUrl: 'dialog1.tmpl.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose: true
+      })
+        .then(function (answer) {
+        }, function () {
+        });
+      //$scope.$watch(function () {
+      //  return $mdMedia('xs') || $mdMedia('sm');
+      //}, function (wantsFullScreen) {
+      //  $scope.customFullscreen = (wantsFullScreen === true);
+      //});
+    };
+  }]);
 
-}]);
+function DialogController($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+}
